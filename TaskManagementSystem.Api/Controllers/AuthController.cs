7a46e454810e0d12fd2b1c8241158b23809c0752
RidvanOzturk <ozturkridvan1@gmail.com>
2024-11-18 14:16:15 +0300

    added loginPage

---
+++ TaskManagementSystem.Api/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagementSystem.Api.Controllers;
+
+public class AuthController : Controller
+{
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+}
